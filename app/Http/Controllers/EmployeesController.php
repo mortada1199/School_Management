214a81fa1b356736196d23985d21e3993398ba68
walieldin.hassan <creaspo6@gmail.com>
2023-03-23 14:00:42 +0200

    index file

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -2,11 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CreateEmployeeRequest;
-use App\Http\Requests\UpdateEmployeeRequest;
-use App\Http\Services\EmployeesServices;
 use App\Models\Employee;
-use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
@@ -30,16 +26,16 @@ class EmployeesController extends Controller
      */
     public function create()
     {
-        
+
         return view('add-employee');
     }
 
 
- 
+
     public function edit()
     {
         return view('edit-employee');
     }
 
-    
+
 }
