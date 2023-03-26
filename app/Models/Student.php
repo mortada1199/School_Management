2bf08f15684c9eb2bd5567ffcc8a4c1114dccc84
murtada.abdalgalil <murtada199815@gmail.com>
2023-03-26 13:44:23 +0200

    add commit api

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Student extends Model
 
+class Student extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
 
    protected $fillable =['name','phone','grade_id','user_id','email','password','age','another_phone'];
