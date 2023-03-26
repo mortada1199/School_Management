ff2cb12e2eff4ddae910bbbaf0ed691f3cf32c47
murtada.abdalgalil <murtada199815@gmail.com>
2023-03-26 12:23:56 +0200

    add function register and login to student api

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::post('student/register',[StudentsController::class,'create']);
+
+Route::post('student/login',[StudentsController::class,'login']);
